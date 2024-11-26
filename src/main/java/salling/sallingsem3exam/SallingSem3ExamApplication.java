9b4e1b23258574531f79c47dc7faf873a8aec5b6
OskarThormar <osth0002@stud.kea.dk>
2024-11-26 11:06:41 +0100

    oprettelse

---
+++ src/main/java/salling/sallingsem3exam/SallingSem3ExamApplication.java
@@ -0,0 +1,13 @@
+package salling.sallingsem3exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SallingSem3ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SallingSem3ExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/salling/sallingsem3exam/SallingSem3ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package salling.sallingsem3exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SallingSem3ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
